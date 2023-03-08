e0e4b720e695446acf7ebc645f274cc5559c352f
Yonas Tamirat <120226721+Yonassiyum@users.noreply.github.com>
2023-03-08 12:51:52 -0500

    first commit

--- Server.js
+++ Server.js
@@ -2,40 +2,30 @@ const express = require("express");
 const app = express();
 const server = require("http").Server(app);
 const io = require("socket.io")(server);
-
 const { ExpressPeerServer } = require("peer");
 const { v4: uuidV4 } = require("uuid");
-
 const peerServer = ExpressPeerServer(server, {
 	debug: true,
 });
-
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use("/peerjs", peerServer);
-
 app.get("/", (req, res) => {
 	res.redirect(`/${uuidV4()}`);
 });
-
 app.get("/:room", (req, res) => {
 	res.render("room", { roomId: req.params.room });
 });
-
 io.on("connection", (socket) => {
-	socket.on("join-room", (roomId,userId) => {
+	socket.on("join-room", (roomId, userId) => {
 		socket.join(roomId);
 		socket.to(roomId).emit("user-connected", userId);
-
 		socket.on("disconnect", () => {
 			socket.to(roomId).emit("user-disconnected", userId);
 		});
-		// console.log("yonas join", roomId);
 		socket.on("message", (message) => {
 			io.to(roomId).emit("createMessage", message);
 		});
-
-		
 	});
 });
 
